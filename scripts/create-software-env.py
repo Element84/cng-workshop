5a4dada4e3b8fc7073d1efa28b04df7b33a98141
jkeifer <jkeifer@element84.com>
2023-10-19 14:28:47 -0700

    script to create coiled software env

---
+++ scripts/create-software-env.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import coiled
+
+ENV_NAME = "cngeo-workshop"
+CONTAINER = "public.ecr.aws/q2i2x3t4/e84-sandbox/coiled-demo"
+
+coiled.create_software_environment(
+    name=ENV_NAME,
+    container=CONTAINER,
+    force_rebuild=True,
+)

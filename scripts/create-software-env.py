3b8021180d8864ffc12137442ef8645bbbac3134
jkeifer <jkeifer@element84.com>
2023-10-19 15:28:22 -0700

    resolve env name discrepancy

--- scripts/create-software-env.py
+++ scripts/create-software-env.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import coiled
 
-ENV_NAME = "cngeo-workshop"
+ENV_NAME = "cng-workshop"
 CONTAINER = "public.ecr.aws/q2i2x3t4/e84-sandbox/coiled-demo"
 
 coiled.create_software_environment(
